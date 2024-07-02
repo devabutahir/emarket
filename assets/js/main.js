11c61e207afd5000bdbd408a72b3f4fff6259d46
devabutahir <devtahir242@gmail.com>
2024-07-02 16:14:03 +0600

    footer-start

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,9 @@ $(document).ready(() => {
 //Time CountDown
 const countdownDates = [
     { date: new Date("Jul 29, 2024 15:00:00").getTime(), daysId: 'days1', hoursId: 'hours1', minutesId: 'minutes1', secondsId: 'seconds1' },
-    // { date: new Date("Dec 31, 2024 23:59:59").getTime(), daysId: 'days2', hoursId: 'hours2', minutesId: 'minutes2', secondsId: 'seconds2' },
-    // { date: new Date("Jan 1, 2025 00:00:00").getTime(), daysId: 'days3', hoursId: 'hours3', minutesId: 'minutes3', secondsId: 'seconds3' },
-    // { date: new Date("Feb 14, 2025 00:00:00").getTime(), daysId: 'days4', hoursId: 'hours4', minutesId: 'minutes4', secondsId: 'seconds4' },
+    { date: new Date("Dec 31, 2024 23:59:59").getTime(), daysId: 'days2', hoursId: 'hours2', minutesId: 'minutes2', secondsId: 'seconds2' },
+    { date: new Date("Jan 1, 2025 00:00:00").getTime(), daysId: 'days3', hoursId: 'hours3', minutesId: 'minutes3', secondsId: 'seconds3' },
+    { date: new Date("Feb 14, 2025 00:00:00").getTime(), daysId: 'days4', hoursId: 'hours4', minutesId: 'minutes4', secondsId: 'seconds4' },
     // { date: new Date("Mar 17, 2025 00:00:00").getTime(), daysId: 'days5', hoursId: 'hours5', minutesId: 'minutes5', secondsId: 'seconds5' },
     // { date: new Date("Apr 1, 2025 00:00:00").getTime(), daysId: 'days6', hoursId: 'hours6', minutesId: 'minutes6', secondsId: 'seconds6' },
     // { date: new Date("May 5, 2025 00:00:00").getTime(), daysId: 'days7', hoursId: 'hours7', minutesId: 'minutes7', secondsId: 'seconds7' }
@@ -63,11 +63,11 @@ let supportfeature = new Swiper(".support-feature-wrap", {
         slidesPerView: 3,
         spaceBetween: 10,
       },
-      600: {
+      768: {
         slidesPerView: 3,
         spaceBetween: 10,
       },
-      400: {
+      767: {
         slidesPerView: 2,
         spaceBetween: 5,
       },
@@ -112,11 +112,11 @@ let flashdealswrap = new Swiper(".flash-deals-wrap", {
       slidesPerView: 4,
       spaceBetween: 10,
     },
-    600: {
+    768: {
       slidesPerView: 3,
       spaceBetween: 10,
     },
-    400: {
+    767: {
       slidesPerView: 2,
       spaceBetween: 5,
     },
@@ -161,11 +161,11 @@ let flashdealswrap02 = new Swiper(".flash-deals-wrap02", {
       slidesPerView: 4,
       spaceBetween: 10,
     },
-    600: {
+    768: {
       slidesPerView: 3,
       spaceBetween: 10,
     },
-    400: {
+    767: {
       slidesPerView: 2,
       spaceBetween: 5,
     },
@@ -189,7 +189,7 @@ const mediaQuery02 = window.matchMedia('(min-width: 991px)');
 mediaQuery.addListener(handleBreakpointChange02);
 handleBreakpointChange02(mediaQuery02);
 
-// Flash Deals 02 Swiper
+// Flash Deals 03 Swiper
 let auctionallitems = new Swiper(".auction-allitems", {
   spaceBetween: 24,
   speed: 1200,
@@ -210,11 +210,11 @@ let auctionallitems = new Swiper(".auction-allitems", {
       slidesPerView: 4,
       spaceBetween: 10,
     },
-    600: {
+    768: {
       slidesPerView: 3,
       spaceBetween: 10,
     },
-    400: {
+    767: {
       slidesPerView: 2,
       spaceBetween: 5,
     },
@@ -237,6 +237,55 @@ const handleBreakpointChange03 = (e) => {
 const mediaQuery03 = window.matchMedia('(min-width: 991px)');
 mediaQuery.addListener(handleBreakpointChange03);
 handleBreakpointChange03(mediaQuery03);
+
+// Flash Deals 04 Swiper
+let accessoriesinner = new Swiper(".accessories-inner", {
+  spaceBetween: 16,
+  speed: 1200,
+  loop: true,
+  navigation: {
+    nextEl: ".sup-pre",
+    prevEl: ".sup-next",
+  },
+  breakpoints: {
+    1399: {
+      slidesPerView: 6,
+    },
+    1199: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    991: {
+      slidesPerView: 5,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+    },
+  },
+});
+// Function to enable or disable Swiper based on viewport width
+const handleBreakpointChange04 = (e) => {
+  if (e.matches) {
+    // Viewport is 991px or wider
+    accessoriesinner.disable();
+  } else {
+    // Viewport is less than 991px
+    accessoriesinner.enable();
+  }
+};
+const mediaQuery04 = window.matchMedia('(min-width: 991px)');
+mediaQuery.addListener(handleBreakpointChange04);
+handleBreakpointChange04(mediaQuery04);
 
 // Today Offer Slider //
 const offerwrappers = new Swiper(".offer-wrappers", {
@@ -273,42 +322,37 @@ breakpoints: {
 },
 });
 
-
-
-
-
-  // Customer Slider //
-  const customerwrapper = new Swiper(".customer-wrapper", {
-    spaceBetween: 0,
-    speed: 1200,
-    loop: true,
-    effect: 'fade',
-    navigation: {
-      nextEl: ".swiper-button-prevteam",
-      prevEl: ".swiper-button-nextteam",
+// Sponsor Section Slider //
+const sponsorwrapper = new Swiper(".sponsor-wrapper", {
+  spaceBetween: 0,
+  speed: 1200,
+  loop: true,
+  autoplay: {
+  delay: 1500,
+  disableOnInteraction: false,
+  },
+  breakpoints: {
+    1399: {
+      slidesPerView: 8,
     },
-    breakpoints: {
-      1399: {
-        slidesPerView: 1,
-      },
-      991: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      600: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      400: {
-        slidesPerView: 1,
-        spaceBetween: 5,
-      },
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 5,
-      },
+    991: {
+      slidesPerView: 7,
+      spaceBetween: 16,
     },
-  });
+    600: {
+      slidesPerView: 6,
+      spaceBetween: 16,
+    },
+    400: {
+      slidesPerView: 4,
+      spaceBetween: 16,
+    },
+    0: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+  },
+});
 
   
   // Testimonial Section Version 01 //
